b3159db8b1d7c98a1c5d3ec6c9600f2d809a72a9
Naowak <yannis.bendiouis@gmail.com>
2024-10-30 16:28:35 +0100

    wip to prepare benchmark usageé

--- RAU.py
+++ RAU.py
@@ -25,7 +25,8 @@ class RAU:
         self.leak_rate = leak_rate
         self.reservoir_kind = reservoir_kind
 
-        self.Win, self.W, self.bias = self._init_weights(input_dim, units, degree, spectral_radius)
+        self.Win, self.W, self.bias, self.Win_mask, self.W_mask = self._init_weights(
+            input_dim, units, degree, spectral_radius)
         self.readout = None
         
     def _init_weights(self, input_dim, units=100, degree=3, spectral_radius=1):
@@ -41,7 +42,12 @@ class RAU:
         tuple : Un tuple contenant trois np.ndarray, Win, W et bias, qui sont les poids de l'entrée, du réservoir et du biais respectivement.
         """
         # Win : connexion entre l'entrée et les unités du réservoir
-        Win = np.random.binomial(1, 1, (units, input_dim)) # /!\ p=1 pour avoir des 1 partout
+        Win_mask = np.zeros((units, input_dim))
+        for i in range(units):
+            indices = np.random.choice(input_dim, degree, replace=False)
+            Win_mask[i, indices] = 1
+        Win = np.zeros((units, input_dim))
+        Win[Win_mask == 1] = 1
 
         # W : connexion entre les unités du réservoir
         W_mask = np.zeros((units, units))
@@ -56,7 +62,7 @@ class RAU:
         # Bias : biais des unités du réservoir
         bias = np.random.normal(0, 1, (units, 1))
 
-        return Win, W, bias
+        return Win, W, bias, Win_mask, W_mask
 
     def _compute_activity(self, X):
         """
@@ -68,37 +74,46 @@ class RAU:
         Retourne :
         np.ndarray : L'activité du réseau.
         """
-        x = np.zeros((self.units, 1)) + self.bias
+        print(X.shape)
+        sample_size = X.shape[0]
+        time = X.shape[1]
+        x = np.zeros((sample_size, self.units, 1)) + self.bias
         activity = []
-        for i in range(len(X)):
-            queries = self.Win @ X[i].reshape(-1, 1)
-            keys = self.W @ x
+        for i in range(time):
+            queries = (self.Win * X[:, i, :].reshape(sample_size, 1, self.input_dim))[:, self.Win_mask != 0].reshape(sample_size, self.units, self.degree)
+            keys = (self.W * x.reshape(sample_size, 1, self.units))[:, self.W_mask != 0].reshape(sample_size, self.units, self.degree)
             if self.reservoir_kind == 'single_attention':
-                x = x * (1 - self.leak_rate) + np.tanh(np.sum(queries * keys, axis=1).reshape(-1, 1) + self.bias) * self.leak_rate
+                x = x * (1 - self.leak_rate) + np.tanh(np.sum(queries * keys, axis=2).reshape(sample_size, self.units, 1) + self.bias) * self.leak_rate
             elif self.reservoir_kind == 'multiple_attention':
                 x = x * (1 - self.leak_rate) + np.tanh(np.sum(queries @ keys.T).reshape(-1, 1) + self.bias) * self.leak_rate
-                #x = x * (1 - self.leak_rate) + np.tanh(np.max(queries @ keys.T).reshape(-1, 1) + self.bias) * self.leak_rate
+                x = x * (1 - self.leak_rate) + np.tanh(np.max(queries @ keys.T).reshape(-1, 1) + self.bias) * self.leak_rate
+                pass
             elif self.reservoir_kind == 'no_attention':
                 inputs = np.concatenate([queries, keys], axis=1)
                 x = x * (1 - self.leak_rate) + np.tanh(np.sum(inputs, axis=1).reshape(-1, 1) + self.bias) * self.leak_rate
             else:
                 raise ValueError('Invalid reservoir kind')
             activity.append(x)
-        activity = np.array(activity)
+        activity = np.array(activity).reshape(sample_size, time, self.units)
 
         return activity
     
     def train(self, X, Y):
+        # Check X dimensions
+        if len(X.shape) != 3:
+            raise ValueError(f"X must have 3 dimensions (sample, time, dim), got {len(X.shape)}")
+
         # Compute activity for X with attention units
-        activity = self._compute_activity(X)
+        activity = self._compute_activity(X) # (sample, time, units)
 
         # Train the readout
         self.readout = Ridge(alpha=1e-7)
         self.readout.fit(activity.reshape(-1, self.units), Y.reshape(-1, self.output_dim))
 
     def run(self, X):
-        activity = self._compute_activity(X)
-        return self.readout.predict(activity.reshape(-1, self.units))
+        activity = self._compute_activity(X) # (sample, time, units)
+        preds = self.readout.predict(activity.reshape(-1, self.units))
+        return preds.reshape(X.shape[0], X.shape[1], self.output_dim)
 
 
 
