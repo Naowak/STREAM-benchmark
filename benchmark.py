bcc4febd262c772c87a53bf4d553d880143fac58
Naowak <yannis.bendiouis@gmail.com>
2024-11-13 15:59:25 +0100

    add requirements to create venv, and report is now generating automatically and takes the max ACCURACY or MSE depending on classification value

--- benchmark.py
+++ benchmark.py
@@ -1,14 +1,14 @@
-import numpy as np
 from typing import Dict, Any, Callable, List, Tuple, Optional
-import time
-import psutil
-import torch
+from sklearn.metrics import accuracy_score, root_mean_squared_error, precision_score, recall_score
 from dataclasses import dataclass
 from collections import defaultdict
-import logging
-from enum import Enum
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, root_mean_squared_error, precision_score, recall_score
+import pandas as pd
+import numpy as np
+import time
+import psutil
+import logging
+import os
 
 @dataclass
 class Task:
@@ -23,21 +23,61 @@ class Task:
 @dataclass
 class TaskResult:
     task_name: str
-    accuracy: float  # Pour les tâches de classification
-    mse: float      # Pour les tâches de régression
-    precision: Optional[float]  # Pour les tâches de classification
-    recall: Optional[float]     # Pour les tâches de classification
+    is_classification: bool
+    mse: float                  # régression
+    accuracy: float             # classification
+    precision: Optional[float]  # classification
+    recall: Optional[float]     # classification
     training_time: float
     inference_time: float
     memory_usage: float
-    training_iterations: int
-    sequence_length: int
-    feature_dim: int
+    task_params: Dict[str, Any]
+    model_params: Dict[str, Any]
+    training_params: Dict[str, Any]
+    # parameters: int
+
+classification_task_template = """
+#### Results
+- Accuracy: {}
+- Precision: {}
+- Recall: {}
+- Training Time: {}
+- Inference Time: {}
+- Memory Usage: {}
+
+#### Task Parameters
+{}
+
+#### Model Parameters
+{}
+
+#### Training Parameters
+{}
+
+"""
+
+regression_task_template = """
+#### Results
+- MSE: {}
+- Training Time: {}
+- Inference Time: {}
+- Memory Usage: {}
+
+#### Task Parameters
+{}
+
+#### Model Parameters
+{}
+
+#### Training Parameters
+{}
+
+"""
 
 class BenchmarkSuite:
-    def __init__(self, model_class: Any, model_name: str, seed: int = 42):
+    def __init__(self, model_class: Any, model_name: str, seeds: list[int] = [42, 43, 44]):
         # Set seed
-        self.seed = seed
+        self.seeds = seeds
 
         # Initialize model_class, tasks and results
         self.model_class = model_class
@@ -66,25 +106,32 @@ class BenchmarkSuite:
         # Pour chaque tâche
         for task in self.tasks.values():
             self.logger.info(f"Evaluating task: {task.name}")
+
+            # Pour chaque seed
+            for seed in self.seeds:
+
+                # Set seed 
+                self.logger.info(f"Seed: {seed}")
+                np.random.seed(seed) 
             
-            # Génération des données
-            try:
-                np.random.seed(self.seed) # Reset seed 
-                X_train, Y_train, X_test, Y_test = task.generator(**(task.generator_params or {}))
-            except Exception as e:
-                self.logger.error(f"\033[91mError generating dataset for task {task.name}: {e}\033[0m")
-                continue
+                # Génération des données
+                try:
+                    X_train, Y_train, X_test, Y_test = task.generator(**(task.generator_params or {}))
+                except Exception as e:
+                    self.logger.error(f"\033[91mError generating dataset for task {task.name}: {e}\033[0m")
+                    continue
 
-            # Entraînement et évaluation du modèle pour un ensemble d'hyperparamètres aléatoires
-            for i in range(task.n_trials):
+                # Pour chaque essai
+                for i in range(task.n_trials):
+                    self.logger.info(f"Trial {i+1}/{task.n_trials}")
 
-                # Randomly select model hyperparameters
-                model_hp = self._select_random_hyperparameters(task)
-                print(model_hp)
+                    # Randomly select model hyperparameters
+                    model_hp = self._select_random_hyperparameters(task)
 
-                # Evaluate model with hyperparameters
-                self._evaluate_model_with_hp(X_train, Y_train, X_test, Y_test, task, model_hp)
+                    # Evaluate model with hyperparameters
+                    self._evaluate_model_with_hp(X_train, Y_train, X_test, Y_test, task, model_hp)
 
+            self.logger.info(f"Completed evaluation for task: {task.name}")
 
 
     def _select_random_hyperparameters(self, task: Task) -> Dict[str, Any]:
@@ -94,7 +141,7 @@ class BenchmarkSuite:
             if type(hp_values) in [int, float]: # Unique value, no choice
                 model_hp[hp_name] = hp_values
             elif type(hp_values) == list and len(hp_values) == 2: # Range of values
-                model_hp[hp_name] = np.random.choice(list(range(hp_values[0], hp_values[1]+1)))
+                model_hp[hp_name] = int(np.random.choice(list(range(hp_values[0], hp_values[1]+1))))
             else: # Error
                 raise ValueError(f"Invalid hyperparameter values for task {task.name}: {hp_values}")
         return model_hp
@@ -133,6 +180,7 @@ class BenchmarkSuite:
         # Enregistrement des résultats
         result = TaskResult(
             task_name=task.name,
+            is_classification=task.is_classification,
             accuracy=metrics['accuracy'],
             mse=metrics['mse'],
             precision=metrics['precision'],
@@ -140,92 +188,120 @@ class BenchmarkSuite:
             training_time=training_time,
             inference_time=inference_time,
             memory_usage=memory_used,
-            training_iterations=getattr(model, 'n_iterations', 0),
-            sequence_length=X_train.shape[1],
-            feature_dim=X_train.shape[2]
+            task_params=task.generator_params,
+            model_params=model_hp,
+            training_params=task.training_params,
         )
-        
-        self.results[self.model_name].append(result)
-        self.logger.info(f"Completed evaluation for task: {task.name}")
-
+        self.results[task.name].append(result)
 
 
     def _evaluate_predictions(self, y_true: np.ndarray, y_pred: np.ndarray, is_classification: bool) -> Dict[str, float]:
         """Évalue les prédictions selon le type de tâche"""
         if is_classification:
             # Convert to class indices if needed
-            if len(y_true.shape) > 2:
-                y_true = np.argmax(y_true, axis=-1)
-            if len(y_pred.shape) > 2:
-                y_pred = np.argmax(y_pred, axis=-1)
+            y_true = (y_true > 0.5).astype(int)
+            y_pred = (y_pred > 0.5).astype(int)
                 
             return {
                 'accuracy': accuracy_score(y_true.flatten(), y_pred.flatten()),
                 'precision': precision_score(y_true.flatten(), y_pred.flatten(), average='weighted', zero_division=0),
                 'recall': recall_score(y_true.flatten(), y_pred.flatten(), average='weighted', zero_division=0),
-                'mse': root_mean_squared_error(y_true, y_pred)
+                'mse': None  # Non applicable pour la classification
             }
         else:
             return {
-                'accuracy': 0.0,  # Non applicable pour la régression
+                'accuracy': None,  # Non applicable pour la régression
                 'precision': None,
                 'recall': None,
-                'mse': root_mean_squared_error(y_true, y_pred)
+                'mse': root_mean_squared_error(y_true.flatten(), y_pred.flatten())
             }
+        
 
-
-
-
-
-    # def generate_report(self, output_path: str = 'benchmark_results'):
-    #     """Génère un rapport détaillé des résultats"""
-    #     import pandas as pd
+    def generate_report(self, output_path: str='benchmark_results'):
+        """Génère un rapport détaillé des résultats.
+        aggregate: 'mean', 'median' or 'max'"""
         
-    #     # Création du dossier de sortie
-    #     import os
-    #     os.makedirs(output_path, exist_ok=True)
+        # Création du dossier de sortie
+        os.makedirs(output_path, exist_ok=True)
         
-    #     # Conversion des résultats en DataFrame
-    #     results_data = []
-    #     for model_name, task_results in self.results.items():
-    #         for result in task_results:
-    #             results_data.append({
-    #                 'Model': model_name,
-    #                 'Task': result.task_name,
-    #                 'Accuracy': result.accuracy,
-    #                 'MSE': result.mse,
-    #                 'Precision': result.precision,
-    #                 'Recall': result.recall,
-    #                 'Training Time (s)': result.training_time,
-    #                 'Inference Time (s)': result.inference_time,
-    #                 'Memory Usage (MB)': result.memory_usage,
-    #                 'Training Iterations': result.training_iterations,
-    #                 'Sequence Length': result.sequence_length,
-    #                 'Feature Dimension': result.feature_dim
-    #             })
+        # Conversion des résultats en DataFrame
+        results_data = []
+        for task_results in self.results.values():
+            for result in task_results:
+                results_data.append({
+                    'Model': self.model_name,
+                    'Task': result.task_name,
+                    'Accuracy': result.accuracy,
+                    'MSE': result.mse,
+                    'Precision': result.precision,
+                    'Recall': result.recall,
+                    'Training Time (s)': result.training_time,
+                    'Inference Time (s)': result.inference_time,
+                    'Memory Usage (MB)': result.memory_usage,
+                    'Task Params': result.task_params,
+                    'Model Params': result.model_params,
+                    'Training Params': result.training_params,
+                })
         
-    #     df = pd.DataFrame(results_data)
+        df = pd.DataFrame(results_data)
         
-    #     # Sauvegarde des résultats bruts
-    #     df.to_csv(f'{output_path}/results.csv', index=False)
+        # Sauvegarde des résultats bruts
+        df.to_csv(f'{output_path}/results.csv', index=False)
         
-    #     # Génération des graphiques
-    #     self._generate_plots(df, output_path)
+        # Génération des graphiques
+        #self._generate_plots(df, output_path)
         
-    #     # Génération du rapport markdown
-    #     with open(f'{output_path}/report.md', 'w') as f:
-    #         f.write('# Benchmark Results\n\n')
+        # Génération du rapport markdown
+        with open(f'{output_path}/report.md', 'w') as f:
+            f.write('# Benchmark Results\n\n')
             
-    #         # Résumé global
-    #         f.write('## Global Performance Summary\n\n')
-    #         summary = df.groupby('Model').mean()
-    #         f.write(summary.to_markdown() + '\n\n')
+            # Résumé global
+            f.write('## Global Performance Summary\n\n')
+
+            # Extrait le meilleur essai pour chaque tâches
+            dfnp = df.drop(['Task Params', 'Model Params', 'Training Params'], axis=1)
+            groups = dfnp.groupby(['Task', 'Model'])
+            best_idx_acc = groups['Accuracy'].idxmax()
+            best_idx_mse = groups['MSE'].idxmin()
+            best_idx = pd.concat([best_idx_acc, best_idx_mse]).dropna()
+
+            # Create summary dataframe
+            summary = dfnp[dfnp.index.isin(best_idx)]
+            f.write(summary.to_markdown() + '\n\n')
             
-    #         # Détails par tâche
-    #         for task_name in df['Task'].unique():
-    #             f.write(f'## Task: {task_name}\n\n')
-    #             task_df = df[df['Task'] == task_name]
-    #             f.write(task_df.to_markdown() + '\n\n')
+            # Détails par tâche
+            best = df[df.index.isin(best_idx)]
+            for task_name in summary['Task']:
+                f.write(f'## Task: {task_name}')
+                task_df = best[best['Task'] == task_name].iloc[0]
+
+                # Classification task
+                if not np.isnan(task_df['Accuracy']):
+                    f.write(classification_task_template.format(
+                        task_df['Accuracy'], 
+                        task_df['Precision'], 
+                        task_df['Recall'], 
+                        task_df['Training Time (s)'], 
+                        task_df['Inference Time (s)'], 
+                        task_df['Memory Usage (MB)'], 
+                        task_df['Task Params'], 
+                        task_df['Model Params'], 
+                        task_df['Training Params']
+                    ))
+                
+                # Régression task
+                elif not np.isnan(task_df['MSE']):
+                    f.write(regression_task_template.format(
+                        task_df['MSE'],
+                        task_df['Training Time (s)'],
+                        task_df['Inference Time (s)'],
+                        task_df['Memory Usage (MB)'], 
+                        task_df['Task Params'], 
+                        task_df['Model Params'], 
+                        task_df['Training Params']
+                    ))
+                else:
+                    raise ValueError('Invalid task type')
 
     # def _generate_plots(self, df: 'pd.DataFrame', output_path: str):
     #     """Génère des visualisations des résultats"""
